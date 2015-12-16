2471711fb5eda28c9fcde5b422d56b8caddec10c
future-challenger <sunshine_anycall@hotmail.com>
2015-12-16 21:55:24 +0800

    books

--- app/src/main/java/book/cn/bookmanagement/MainActivity.java
+++ app/src/main/java/book/cn/bookmanagement/MainActivity.java
@@ -1,7 +1,8 @@
 package book.cn.bookmanagement;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                
+            }
+        }, 1000);
     }
 }
