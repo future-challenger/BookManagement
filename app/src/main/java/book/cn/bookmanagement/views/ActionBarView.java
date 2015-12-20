08015d76da2428f729c25a6f070eff5db73e2075
future-challenger <sunshine_anycall@hotmail.com>
2015-12-20 12:11:05 +0800

    register & login

--- app/src/main/java/book/cn/bookmanagement/views/ActionBarView.java
+++ app/src/main/java/book/cn/bookmanagement/views/ActionBarView.java
@@ -1,11 +1,13 @@
 package book.cn.bookmanagement.views;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import book.cn.bookmanagement.R;
 
@@ -32,9 +34,43 @@ public class ActionBarView extends LinearLayout implements View.OnClickListener 
         View v = inflater.inflate(R.layout.action_bar_view, this);
         Button leftButton = (Button) v.findViewById(R.id.leftButton);
         Button rightButton = (Button) v.findViewById(R.id.rightButton);
+        TextView titleTextView = (TextView) v.findViewById(R.id.titleView);
 
         leftButton.setOnClickListener(this);
         rightButton.setOnClickListener(this);
+
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.ActionBarView);
+        if (a != null) {
+            boolean hideLeftButton = a.getBoolean(R.styleable.ActionBarView_hideLeftButton, false);
+            if (!hideLeftButton) {
+                leftButton.setVisibility(View.VISIBLE);
+                
+                String leftString = a.getString(R.styleable.ActionBarView_leftButtonTitle);
+                leftButton.setText(leftString);
+            } else {
+                leftButton.setVisibility(View.INVISIBLE);
+            }
+
+            boolean hideRightButton = a.getBoolean(R.styleable.ActionBarView_hideRightButton, false);
+            if (!hideRightButton) {
+                rightButton.setVisibility(View.VISIBLE);
+
+                String rightString = a.getString(R.styleable.ActionBarView_rightButtonTitle);
+                rightButton.setText(rightString);
+            } else {
+                rightButton.setVisibility(View.INVISIBLE);
+            }
+
+            boolean hideTitleTextView = a.getBoolean(R.styleable.ActionBarView_hideTitleString, false);
+            if (!hideTitleTextView) {
+                titleTextView.setVisibility(View.VISIBLE);
+
+                String titleString = a.getString(R.styleable.ActionBarView_titleString);
+                titleTextView.setText(titleString);
+            } else {
+                titleTextView.setVisibility(View.INVISIBLE);
+            }
+        }
     }
 
     @Override
