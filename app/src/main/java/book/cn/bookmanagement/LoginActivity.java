b7811ea1694850726edbca4a1f81d6459a9bf128
future-challenger <sunshine_anycall@hotmail.com>
2015-12-20 13:10:27 +0800

    okhttp added

--- app/src/main/java/book/cn/bookmanagement/LoginActivity.java
+++ app/src/main/java/book/cn/bookmanagement/LoginActivity.java
@@ -1,13 +1,50 @@
 package book.cn.bookmanagement;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+
+import java.io.IOException;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private OkHttpClient _client = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        Button rightButton = (Button) findViewById(R.id.registerButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+            }
+        });
+        rightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
+    String run(String url) throws IOException {
+        Request request = new Request.Builder().url(url).build();
+        com.squareup.okhttp.Response response = _client.newCall(request).execute();
+        return response.body().string();
+    }
+
+
 }
---
+++ app/src/main/java/book/cn/bookmanagement/Utils/HttpUtil.java
@@ -0,0 +1,7 @@
+package book.cn.bookmanagement.Utils;
+
+/**
+ * Created by uncle_charlie on 20/12/15.
+ */
+public class HttpUtil {
+}
